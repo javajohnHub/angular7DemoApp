9b58df1c04d886690c67c39a4072e41dafb5f854
John Edwards <joedw1978@gmail.com>
2018-10-21 18:17:13 -0400

    giphy done

--- src/app/components/giphy/giphy-result.component.ts
+++ src/app/components/giphy/giphy-result.component.ts
@@ -8,12 +8,6 @@ import {GiphyResult} from '../../models/giphy-result';
   template: `
    <div class="col-sm-6 col-md-3">
         <img class="img-responsive img-thumbnail" src="{{result.image}}">
-        <div class="caption">
-          <h3>Rating: {{result.rating}}</h3>
-          <p>Submitted by: {{result.username}}</p>
-          <p>Submitted: {{result.import_datetime}}</p>
-          <p>Trending: {{result.trending_datetime}}</p>
-        </div>
     </div>
   `
 })
--- src/app/components/giphy/giphy-search.component.ts
+++ src/app/components/giphy/giphy-search.component.ts
@@ -5,19 +5,18 @@ import {GiphyResult} from '../../models/giphy-result';
   selector: 'app-giphy-search',
   template: `
 
-        <div class="form-group well">
-          <app-giphy-search-box
+  <app-giphy-search-box
             (loading)="loading = $event"
             (results)="updateResults($event)"
           ></app-giphy-search-box>
-        </div>
-
-      <div *ngIf="results" class="search-res">
-        <app-giphy-result
-          *ngFor="let result of results"
+<div *ngIf="results">
+<mat-grid-list cols="3" cols-md="2" cols-sm="1" gutter="12px" row-height="3:3">
+<mat-grid-tile *ngFor="let result of results">
+<app-giphy-result
           [result]="result">
         </app-giphy-result>
-      </div>
+</mat-grid-tile>
+</mat-grid-list>
   `,
 })
 export class GiphySearchComponent {
